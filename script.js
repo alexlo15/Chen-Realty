2132e6881c06ee6b35fad88a77aabb0c6714395c
Alex Lo <loalexander389@gmail.com>
2019-07-29 12:45:32 -0400

    formatting created different corresponding pages, much to do

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+
 window.addEventListener("load", () => {
  document.querySelector("body").classList.add("loaded"); 
 });
+
+
+// $("#but1").on("click", function () {
+//     $("body").removeClass()
+//     $("body").addClass("space");
+//     $("nav").css("background-image", "linear-gradient(-180deg, #5500c3, #283643)");
+//     $("footer").css("background-image", "linear-gradient(-180deg, #283643, #5500c3)");
+//     $("footer").css("color", "rgb(241, 155, 208)");
+//     $("button.alo").removeClass("btn-success", "btn-primary", "btn-warning", "btn-link", "btn-dark", "btn-info", "btn-danger", "btn-light");
+//     $("button.alo").addClass("btn-secondary");
+// });
+
+
+
